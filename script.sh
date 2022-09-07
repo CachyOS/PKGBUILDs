478de6cbd79fa3ca8be039c070d041e697c16732
ptr1337 <admin@ptr1337.dev>
2022-09-07 10:34:05 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,10 +6,11 @@ for f in $files
 do
         d=$(dirname $f)
         cd $d
-        docker run --name ci-build -e EXPORT_PKG=1 -e USE_PARU=1 -e SYNC_DATABASE=1 -e IGNORE_ARCH=1 -v /tmp/docker-makepkg-v3:/tmp/makepkg -v $PWD:/pkg -v /home/ptr1337/ccache:/home/notroot/.buildcache pttrr/docker-makepkg-v3
+        docker run --name ci-build -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e IGNORE_ARCH=1 -v /tmp/docker-makepkg-v3:/tmp/makepkg -v $PWD:/pkg -v /home/ptr1337/ccache:/home/notroot/.buildcache pttrr/docker-makepkg-v3
         docker rm ci-build
         cd ..
 done
 
 mv */*-x86_64_v3.pkg.tar.zst* /home/ptr1337/.docker/build/nginx/www/repo/x86_64_v3/cachyos-v3/
 update-repo-v3
+repoctl update -P cachyos-v3
